a1d0037a00b467946afb76092a9016c9804844a8
moumena-allawi <moumenaallawi@gmail.com>
2026-09-03 14:12:16 +0300

    Refactor code structure for improved readability and maintainability

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/moumena', [ TestController::class, 'index']);
+
+
+Route::get('/printName/{name?}', [TestController::class, 'printName']);
