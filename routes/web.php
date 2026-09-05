ab9de12d0d8ad9915685562a03da206f02ca005d
moumena-allawi <moumenaallawi@gmail.com>
2026-09-05 21:15:38 +0300

    Add SingleController and CheckIfNameIsMassa middleware; update app and web routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SingleController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\PostController;
+use App\Http\Middleware\CheckIfNameIsMassa;
+use Illuminate\Support\Facades\Route; 
 
 
 /*Route::get('/', function () {
     return view('welcome');
 });*/
-Route::get('/moumena', [ TestController::class, 'moumena']);
+//Route::get('/moumena', [ TestController::class, 'moumena']);
 
+Route::get('/hello' , SingleController::class)->middleware('check_name');
 
-Route::get('/print-name/{name?}', [TestController::class, 'printName'])->name('printMyNameRoute');
+//Route::get('/print-name/{name?}', [TestController::class, 'printName'])->name('printMyNameRoute');
 
-Route::get('/hello' , [UserController::class, 'index']);
+//Route::get('/hello' , [UserController::class, 'index']);
  
-Route::resource('posts', PostController::class);
+//Route::resource('posts', PostController::class);  
